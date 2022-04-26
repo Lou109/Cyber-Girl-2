4420a0041597add6834b0296da43fc389ba8303e
Louise Collins <louisecollins1971@icloud.com>
2022-04-26 01:37:28 +0100

    Horizontal Player Movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,20 +6,27 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     Vector2 moveInput;
+    Rigidbody2D myRigidbody;
+    [SerializeField] float runSpeed = 10f;
 
     void Start()
     {
-        
+        myRigidbody = GetComponent<Rigidbody2D>();
     }
    
     void Update()
     {
-        
+        Run();
     }
 
     void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
-        Debug.Log(moveInput);
+    }
+
+    void Run()
+    {
+        Vector2 playerVelocity = new Vector2(moveInput.x * runSpeed, myRigidbody.velocity.y);
+        myRigidbody.velocity = playerVelocity;
     }
 }
