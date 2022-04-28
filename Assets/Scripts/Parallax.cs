3cfd6889dfb6d6cb71ac2535c14fdf70609d85a5
Louise Collins <louisecollins1971@icloud.com>
2022-04-28 17:34:17 +0100

    making adjustments to camera

---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    [SerializeField] Transform cam;
+    [SerializeField] float relativeMove = .3f;
+    [SerializeField] bool lockY = false;
+
+    void Update()
+    {
+
+        if (lockY)
+        {
+            transform.position = new Vector2
+           (cam.position.x * relativeMove, transform.position.y);
+        }
+        else
+        {
+            transform.position = new Vector2
+            (cam.position.x * relativeMove, cam.position.y * relativeMove);
+        }
+    }
+
+}
