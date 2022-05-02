1b0ef912080f87f4c34acb5869f47697e1fea50f
Louise Collins <louisecollins1971@icloud.com>
2022-05-02 03:09:30 +0100

    added moving platform

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -6,26 +6,51 @@ public class EnemyMovement : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 1f;
     Rigidbody2D myRigidbody;
+    CapsuleCollider2D capsuleCollider;
+    Health health;
 
-    void Start()
+    void Awake()
     {
         myRigidbody = GetComponent<Rigidbody2D>();
+        capsuleCollider = GetComponent<CapsuleCollider2D>();
     }
-
+    void Start()
+    {
+        health = GetComponent<Health>();
+    }
 
     void Update()
     {
-        myRigidbody.velocity = new Vector2(moveSpeed, 0f);
+        MoveEnemy();
+    }
+    public void MoveEnemy()
+    {
+        int healthAmount = health.GetHealth();
+
+
+        if (healthAmount >= 0)
+        {
+            myRigidbody.velocity = new Vector2(moveSpeed, 0f);
+        }
+
+        else if (healthAmount <= 0)
+        {
+            myRigidbody.velocity = Vector2.zero;
+            capsuleCollider.enabled = false;
+        }
     }
 
     void OnTriggerExit2D(Collider2D collision)
     {
-        moveSpeed = -moveSpeed;
-        FlipEnemyFacing();
-    }
+        if (collision.CompareTag("Ground"))
+        {
+            moveSpeed = -moveSpeed;
+            FlipEnemyFacing();
+        }
 
-    void FlipEnemyFacing()
-    {
-        transform.localScale = new Vector2(-(Mathf.Sign(myRigidbody.velocity.x)), 1f);
+        void FlipEnemyFacing()
+        {
+            transform.localScale = new Vector2(-Mathf.Sign(myRigidbody.velocity.x), 1f);
+        }
     }
 }
